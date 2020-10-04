71ade8fad4f01c48f0cadfc59d4cf4955bc11f91
NeerajGurung <54499483+NeerajGurung@users.noreply.github.com>
2020-10-04 18:56:01 +0545

    Updates

---
+++ precache-manifest.9be7acbbbb0d8b11bd034997d4766914.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47e4363836fa9cf0db6213005200dd72",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "91360bc2515096434660",
+    "url": "/react-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6a10a78df6c76ca3a627",
+    "url": "/react-deploy/static/js/2.b1b31c97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.b1b31c97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91360bc2515096434660",
+    "url": "/react-deploy/static/js/main.0d5d7fca.chunk.js"
+  },
+  {
+    "revision": "9a6b4006b58ad85b50f6",
+    "url": "/react-deploy/static/js/runtime-main.3e11c617.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
